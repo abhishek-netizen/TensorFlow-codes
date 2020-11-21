a2d40a3e5450b4bcd7e59b2e11fadbb61f946be4
abhishek_jn <63057914+abhishek-netizen@users.noreply.github.com>
2020-11-21 23:38:57 +0530

    Add files via upload

---
+++ python20_9_18/cls1.py
@@ -0,0 +1,7 @@
+from cls import Employee
+emp1=Employee('sam','kumar',1000)
+emp2=Employee('john','kumar',1000)
+print(type(emp1))
+print(dir(Employee))
+print(emp1.fullName())
+print(emp2.fullName())	
